71316634e86f4baa57586db17427f92a5be8851d
woohyuk <7dngur7@naver.com>
2024-09-26 16:49:32 +0900

    fix: 시큐리티 열어놓음

--- greatjourney/src/main/java/backend/greatjourney/domain/config/SecurityConfiguration.java
+++ greatjourney/src/main/java/backend/greatjourney/domain/config/SecurityConfiguration.java
@@ -38,8 +38,8 @@ public class SecurityConfiguration {
         http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(request -> request
                         .requestMatchers("/**").permitAll()
-                        .requestMatchers("/api/login/auth/**").permitAll()
-                        .requestMatchers("/api/kakao/auth/**").permitAll()
+                        //.requestMatchers("/api/login/auth/**").permitAll()
+                        //.requestMatchers("/api/kakao/auth/**").permitAll()
                         .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
                         .requestMatchers(swaggerUrls).permitAll()
                         .requestMatchers("/**").hasAuthority(Role.ROLE_USER.name())
