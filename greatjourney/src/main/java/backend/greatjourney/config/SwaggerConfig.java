ac9a3d4c7b09d7a3bf745f5713bcda983659bb4e
조민혁 <71816821+minhyuk2@users.noreply.github.com>
2025-07-17 00:52:50 -0700

    feat : swagger config 추가 #1

--- greatjourney/src/main/java/backend/greatjourney/config/SwaggerConfig.java
+++ greatjourney/src/main/java/backend/greatjourney/config/SwaggerConfig.java
@@ -1,24 +1,40 @@
 package backend.greatjourney.config;
 
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
-import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
-import org.springframework.context.annotation.Bean;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 
+@Configuration
 public class SwaggerConfig {
-//    @Bean
-//    public OpenAPI api() {
-//        SecurityScheme apiKey = new SecurityScheme()
-//                .type(SecurityScheme.Type.APIKEY)
-//                .in(SecurityScheme.In.HEADER)
-//                .name("Authorization");
-//
-//        SecurityRequirement securityRequirement = new SecurityRequirement()
-//                .addList("Bearer Token");
-//
-//        return new OpenAPI()
-//                .components(new Components().addSecuritySchemes("Bearer Token", apiKey))
-//                .addSecurityItem(securityRequirement);
-//    }
+	@Bean
+	public OpenAPI openAPI() {
+		SecurityRequirement securityRequirement = new SecurityRequirement().addList("BearerAuth");
+
+		return new OpenAPI()
+			.components(new Components())
+			.info(apiInfo())
+			.addSecurityItem(securityRequirement)
+			.schemaRequirement("BearerAuth", securityScheme());
+	}
+
+	private Info apiInfo() {
+		return new Info()
+			.title("어디로 API")
+			.description("대장정이팀 API 명세서입니다")
+			.version("1.0.0");
+	}
+
+	private SecurityScheme securityScheme() {
+		return new SecurityScheme()
+			.type(SecurityScheme.Type.HTTP)
+			.scheme("bearer")
+			.bearerFormat("JWT")
+			.in(SecurityScheme.In.HEADER)
+			.name("Authorization");
+	}
 }
